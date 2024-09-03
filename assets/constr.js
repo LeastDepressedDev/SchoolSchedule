7f38738ec3661878676824d0119d22f1e01e2f07
Sirtage <zirocilt@gmail.com>
2024-09-03 04:11:33 +0300

    Updated code and data

--- assets/constr.js
+++ assets/constr.js
@@ -34,12 +34,14 @@ function updateCurrentSchedule() {
         elem.innerHTML = "!!!Выходной!!!";
     } else {
         let day = data["schedule"][nline]["0"][date.getDay()];
-        for (var i = 0; i < 9; i++) {
+        for (var i = 0; i < 11; i++) {
             let tmp = day["pos"][i];
-            elem.innerHTML += `<div style="position: absolute; top: ${i == 0 ? "" : i}5%; width: 96%; height: 10%" class="sm-bordered">
-                        <div style="position: absolute; width: 10%; height: 98%" class="sm-bordered">${i}</div>
+            let cls = tmp == undefined ? "" : data["cabin-reg"][tmp];
+            cls = cls == undefined ? cls : cls[nline];
+            elem.innerHTML += `<div style="position: absolute; top: ${i == 0 ? "" : i*8+4}%; width: 99%; height: 7%" class="sm-bordered">
+                        <div style="position: absolute; width: 10%; height: 98%; text-align: center;" class="sm-bordered">${i}</div>
                         <div style="position: absolute; left: 10%; width: 80%; height: 98%" class="sm-bordered">${tmp == undefined ? "" : tmp}</div>
-                        <div style="position: absolute; left: 90%; width: 10%; height: 98%" class="sm-bordered"></div>
+                        <div style="position: absolute; left: 90%; width: 10%; height: 98%; text-align: center;" class="sm-bordered">${cls == undefined ? "" : cls}</div>
                     </div>
             `;
         }
